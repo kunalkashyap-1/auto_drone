d98da66015465deb408f71e330060cfdb8503a2b
Shiva Kumar <83355044+Shiva02Kumar@users.noreply.github.com>
2022-11-15 12:47:21 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import os
+
+
+def detectAruco(img, markerSize=6, totalMarker=250, draw=True):
+    imgGS = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarker}')
+    arucoDict = aruco.Dictionary_get(key)
+    arucoParams = aruco.DetectorParameters_create()
+    boundBox, ids, rejected = aruco.detectMarkers(imgGS, arucoDict, parameters = arucoParams)
+    print(ids)
+    if draw:
+        aruco.drawDetectedMarkers(img, boundBox)
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, img = cap.read()
+        cv2.imshow("img", img)
+        detectAruco(img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
